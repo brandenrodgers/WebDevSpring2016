39997b300c3f78cea376b60e3aaef810809c0818
Branden Rodgers <branden@rodgersworld.com>
2016-02-18 16:27:43 -0500

    added to experiments page

--- public/experiments/app.js
+++ public/experiments/app.js
@@ -3,13 +3,36 @@
         .module("ExperimentsApp", [])
         .controller("ExperimentsController", ExperimentsController);
 
-    function ExperimentsController($scope) {
-        $scope.header = "Hello World from AngularJS";
+    function ExperimentsController($scope, $http) {
+        $scope.header = "Enter two Numbers to Add Together";
 
         $scope.addIntegers = addIntegers;
+        $scope.search = search;
+        $scope.getMovieData = getMovieData;
 
         function addIntegers(num1, num2) {
             $scope.result = parseInt(num1) + parseInt(num2);
+        }
+
+        function search(title){
+            $http.get("http://www.omdbapi.com/?s=" + title)
+                .success(render);
+        }
+
+        function render(response){
+            $scope.data = response;
+        }
+
+        function getMovieData(imdbID){
+            console.log(imdbID);
+            $http.get("http://www.omdbapi.com/?i=" + imdbID)
+                .success(updateMovieInfo);
+        }
+
+        function updateMovieInfo(response){
+            $scope.movieTitle = response.Title;
+            $scope.movieDirector = response.Director;
+            $scope.moviePlot = response.Plot;
         }
     }
 })();
