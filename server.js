e898b9354537d130db01677886622fe959dd77ef
Branden Rodgers <branden@rodgersworld.com>
2016-03-04 03:10:26 -0500

    not sure why requests aren't working on the openshift server

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ var multer = require('multer');
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 
-var PORT = process.env.OPENSHIFT_NODEJS_PORT || 3000;
-var IPADDRESS = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 
 app.use(multer());
 app.use(bodyParser.json());
@@ -14,11 +14,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use(express.static(__dirname + '/public'));
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://webdevspring2016-rodgersbranden.rhcloud.com");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
 
 app.get('/', function(req, res){
     res.sendfile((__dirname + '/index.html'));
@@ -26,6 +21,6 @@ app.get('/', function(req, res){
 
 require('./public/project/cheapeats/server/app.js')(app);
 
-app.listen(PORT, IPADDRESS, function(){
-    console.log('listening on: ' + IPADDRESS + ':' + PORT);
+app.listen(port, ipaddress, function(){
+    console.log('listening on: ' + ipaddress + ':' + port);
 });
